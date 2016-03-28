9ea8439835aa3b2413d0bf1fe67494809097b6e0
darren90 <1005052145@qq.com>
2016-03-28 22:34:47 +0800

    init SlideView Swift

---
+++ TFSlideView_Swift/TFSlideView_Swift/TFSlideView_Swift/TFSlideView/TFCustomSlideView.swift
@@ -0,0 +1,21 @@
+//
+//  TFCustomSlideView.swift
+//  TFSlideView_Swift
+//
+//  Created by Tengfei on 16/3/28.
+//  Copyright © 2016年 tengfei. All rights reserved.
+//
+
+import UIKit
+
+class TFCustomSlideView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ TFSlideView_Swift/TFSlideView_Swift/TFSlideView_Swift/TFSlideView/TFTableSlideView.swift
@@ -0,0 +1,21 @@
+//
+//  TFTableSlideView.swift
+//  TFSlideView_Swift
+//
+//  Created by Tengfei on 16/3/28.
+//  Copyright © 2016年 tengfei. All rights reserved.
+//
+
+import UIKit
+
+class TFTableSlideView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
