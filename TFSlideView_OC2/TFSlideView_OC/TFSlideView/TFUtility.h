3743929462645670cbaf5d36219f42f496edc1ba
darren90 <1005052145@qq.com>
2016-04-25 22:28:22 +0800

    add

---
+++ TFSlideView_OC2/TFSlideView_OC/TFSlideView/TFUtility.h
@@ -0,0 +1,15 @@
+//
+//  TFUtility.h
+//  TFSlideView_OC
+//
+//  Created by Tengfei on 16/4/25.
+//  Copyright © 2016年 tengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TFUtility : NSObject
+
++ (UIColor *)getColorOfPercent:(CGFloat)percent between:(UIColor *)color1 and:(UIColor *)color2;
+
+@end
