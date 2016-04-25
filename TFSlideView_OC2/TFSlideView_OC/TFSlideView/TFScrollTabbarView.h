44d1feb36dfde9a01c83785da2f24c58a2b86d62
darren90 <1005052145@qq.com>
2016-04-25 23:01:39 +0800

    add

--- TFSlideView_OC2/TFSlideView_OC/TFSlideView/TFScrollTabbarView.h
+++ TFSlideView_OC2/TFSlideView_OC/TFSlideView/TFScrollTabbarView.h
@@ -8,6 +8,36 @@
 
 #import <UIKit/UIKit.h>
 
-@interface TFScrollTabbarView : UIView
+@interface TFScrollTabbarItem :NSObject
 
+@property(nonatomic, strong) NSString *title;
+@property(nonatomic, assign) CGFloat width;
++ (TFScrollTabbarItem *)itemWithTitle:(NSString *)title width:(CGFloat)width;
+
+@end
+
+@protocol TFSlideTabbarDelegate <NSObject>
+
+- (void)DLSlideTabbar:(id)sender selectAt:(NSInteger)index;
+
+@end
+
+
+@interface TFScrollTabbarView : UIView
+@property(nonatomic, strong) UIView *backgroundView;
+
+// tabbar属性
+@property (nonatomic, strong) UIColor *tabItemNormalColor UI_APPEARANCE_SELECTOR;
+@property (nonatomic, strong) UIColor *tabItemSelectedColor UI_APPEARANCE_SELECTOR;
+@property (nonatomic, assign) CGFloat tabItemNormalFontSize;
+@property(nonatomic, strong) UIColor *trackColor UI_APPEARANCE_SELECTOR;
+@property(nonatomic, strong) NSArray *tabbarItems;
+
+// DLSlideTabbarProtocol
+@property(nonatomic, assign) NSInteger selectedIndex;
+@property(nonatomic, readonly) NSInteger tabbarCount;
+@property(nonatomic, weak) id<TFSlideTabbarDelegate> delegate;
+- (void)switchingFrom:(NSInteger)fromIndex to:(NSInteger)toIndex percent:(float)percent;
+
+ 
 @end
