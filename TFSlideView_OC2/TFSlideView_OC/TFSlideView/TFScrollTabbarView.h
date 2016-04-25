8ecff091b2c76fac33f040060d50456b141e6a33
darren90 <1005052145@qq.com>
2016-04-25 22:36:48 +0800

    add

---
+++ TFSlideView_OC2/TFSlideView_OC/TFSlideView/TFScrollTabbarView.h
@@ -0,0 +1,13 @@
+//
+//  TFScrollTabbarView.h
+//  TFSlideView_OC
+//
+//  Created by Tengfei on 16/4/25.
+//  Copyright © 2016年 tengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TFScrollTabbarView : UIView
+
+@end
---
+++ TFSlideView_OC2/TFSlideView_OC/TFSlideView/TFScrollTabbarView.m
@@ -0,0 +1,21 @@
+//
+//  TFScrollTabbarView.m
+//  TFSlideView_OC
+//
+//  Created by Tengfei on 16/4/25.
+//  Copyright © 2016年 tengfei. All rights reserved.
+//
+
+#import "TFScrollTabbarView.h"
+
+@implementation TFScrollTabbarView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
